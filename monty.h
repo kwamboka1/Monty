4fad0d3d36291320f6c2bf4dd5f3b24f7db9e99e
YourDevLebo <djlebzana@gmail.com>
2021-12-02 08:04:18 -0500

    only 3 errors and 1 note left to fix

--- monty.h
+++ monty.h
@@ -4,7 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#include <ctype.h>
+
+#define UNUSED(x) (void)(x)
+#define TRUE 1
+#define FALSE 0
+#define DELIMS "\n \t\r"
 
 /* Global */
 
@@ -15,7 +26,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -31,7 +42,7 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
@@ -39,4 +50,28 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int n_line);
 } instruction_t;
 
+/**
+ * struct glob_s - globally useful variables, all rolled into one
+ * @top: double pointer to top of stack
+ * @ops: double pointer to an instruction struct
+**/
+typedef struct glob_s
+{
+	stack_t **top;
+	instruction_t **ops;
+} glob_t;
+
+extern glob_t glob;
+
+/* monty.c */
+void stack_init(stack_t **head);
+void free_all(void);
+
+int process_file(char *filename, stack_t **stack);
+void call_functions(stack_t **stack, char *op, unsigned int line_number);
+
+void instruction_push(stack_t **stack, unsigned int line_number);
+void instruction_pall(stack_t **stack, unsigned int line_number);
+
+int _strtol(char *num_string, unsigned int line_number);
 #endif
